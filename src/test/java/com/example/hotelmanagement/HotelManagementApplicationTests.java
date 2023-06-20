5ac96f5be1712a3651c9f5e3ee9174ec7d66cac5
HasilbekR <hreymbaev@gmail.com>
2023-06-20 17:59:54 +0500

    Initial commit

---
+++ src/test/java/com/example/hotelmanagement/HotelManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hotelmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

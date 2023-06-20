64188a0da7031ef0d74ce575f35d037f5afc184d
HasilbekR <hreymbaev@gmail.com>
2023-06-20 19:15:08 +0500

    skeleton created

---
+++ src/main/java/com/example/hotelmanagement/repository/RoomRepository.java
@@ -0,0 +1,10 @@
+package com.example.hotelmanagement.repository;
+
+import com.example.hotelmanagement.entity.room.RoomEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+@Repository
+public interface RoomRepository extends JpaRepository<RoomEntity, UUID> {
+}

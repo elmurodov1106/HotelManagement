02c349bafd016e7d832298f9a29968be116fe3e9
javohirjahonov <jahonovjavohir0@gmail.com>
2023-06-21 13:41:35 +0500

    added AuthenticationService, AuthService, BeanConfig, JwtService, JwtTokenFilter, SecurityConfig in project

---
+++ src/main/java/com/example/hotelmanagement/service/AuthService.java
@@ -0,0 +1,22 @@
+package com.example.hotelmanagement.service;
+
+import com.example.hotelmanagement.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+        return userRepository.findUserEntitesByPhoneNumber(userName)
+                .orElseThroe(() -> new DataNotFoundException("User not found")
+                );
+    }
+}
